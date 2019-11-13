8a9f9827d697e05ddcff414a8d1be3704be0a5ff
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2019-11-12 21:15:18 -0500

    inicial commit

---
+++ src/main/java/com/customer/subscription/CustomerSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.customer.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSubscriptionApplication.class, args);
+	}
+
+}
